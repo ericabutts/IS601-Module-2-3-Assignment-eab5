419be76e3e4f4062b5607ea15a9fef85bdfa0de8
eab5 <eab5@njit.edu>
2025-09-13 17:52:48 -0400

    Added calculator file tests

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,20 +1,26 @@
-# import sys
-# from io import StringIO
-# import contextlib
-# from app.calculator import calculator  # adjust import if needed
+import sys
+from io import StringIO
+import contextlib
+from app.calculator import calculator  # adjust import if needed
 
-# def run_calculator_with_input(monkeypatch, inputs):
-#     """Simulate user input.
-#     param monkeypatch: pytest fixture to simulate user input
-#     param inputs: list of strings representing simulated input
-#     return: captured output as a string
-#     """
+def run_calculator_with_input(monkeypatch, inputs):
+    """Simulate user input.
+    param monkeypatch: pytest fixture to simulate user input
+    param inputs: list of strings representing simulated input
+    return: captured output as a string
+    """
 
-#     input_iter = iter(inputs)
-#     monkeypatch.setattr('builtins.input', lambda _: next(input_iter))
+    input_iter = iter(inputs)
+    monkeypatch.setattr('builtins.input', lambda _: next(input_iter))
 
-#     capture_output = StringIO()
-#     with contextlib.redirect_stdout(capture_output):
-#         calculator()
+    capture_output = StringIO()
+    with contextlib.redirect_stdout(capture_output):
+        calculator()
 
-#     return capture_output.getvalue()
+    return capture_output.getvalue()
+
+def test_addition(monkeypatch):
+    """Testing addition for calculator"""
+    inputs = ["addition 1 1", "quit"]
+    output = run_calculator_with_input(monkeypatch, inputs)
+    assert "addition result: 2.0" in output
