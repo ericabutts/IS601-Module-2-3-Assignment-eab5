861c1ca5a843b7d7ce770be2832321c3e46f4faf
eab5 <eab5@njit.edu>
2025-09-22 15:15:58 -0400

    Added the Factory pattern to Calculator

--- app/calculator/__init__.py
+++ app/calculator/__init__.py
@@ -1,7 +1,32 @@
 from app.operations import Operations
 # from calculator import add, subtract, multiply, divide
 
+class Calculation:
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation = operation # reference to function
+    
+    def get_result(self):
+        return self.operation(self.a, self.b)
+    
+class CalculationFactory:
+    @staticmethod
+    def create_calculator(a, b, operation_name):
+        operations = {
+            "addition": Operations.add,
+            "subtraction": Operations.subtract,
+            "multiplication": Operations.multiply,
+            "division": Operations.divide,
+        }
+        operation_function = operations.get(operation_name)
+        if not operation_function:
+            raise ValueError(f"Not a valid operation: {operation_name}. Input in the following format: <operation> <number 1> <number 2>")
+        return Calculation(a, b, operation_function)
+
 def calculator():
+    history = []
+
     while True:
         userinput = input(
             "Input an operation (addition, subtraction, multiplication, division) and two numbers. Type 'quit' to exit: "
@@ -10,6 +35,8 @@ def calculator():
         if userinput.lower() == "quit":
             print("Quitting program")
             break
+        elif userinput.lower() == "help":
+            print("Format: <operation> <num1> <num2> | SUpported operations") # trigger help functions
 
         try:
             operation, num1, num2 = userinput.split()
@@ -20,18 +47,9 @@ def calculator():
             continue
 
         try:
-            if operation == "addition":
-                result = Operations.add(num1, num2)
-            elif operation == "subtraction":
-                result = Operations.subtract(num1, num2)
-            elif operation == "multiplication":
-                result = Operations.multiply(num1, num2)
-            elif operation == "division":
-                result = Operations.divide(num1, num2)
-            else:
-                print(f"Not a valid operation: {operation}. Input in the following format: <operation> <number 1> <number 2>")
-                continue
-
+            calc = CalculationFactory.create_calculator(num1, num2, operation)
+            result = calc.get_result()
+            history.append(f"{operation} {num1} {num2} = {result}")
             print(f"{operation} result: {result}")
 
         except ValueError as e:
