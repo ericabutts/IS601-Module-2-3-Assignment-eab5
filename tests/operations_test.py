4db177845dcb2dc6836d9e883ffe301f3db7c2c2
eab5 <eab5@njit.edu>
2025-09-22 10:09:46 -0400

    Fixed the importing of Operations class from the tests file

--- tests/operations_test.py
+++ tests/operations_test.py
@@ -1,11 +1,11 @@
 import pytest
-from app.operations import add, subtract, multiply, divide
+from app.operations import Operations
 
 def test_addition_positive():
     """Positive tests for addition"""
-    assert add(1,2) == 3
-    assert add(1,3) == 4
-    assert add(1,4) == 5
+    assert Operations.add(1,2) == 3
+    assert Operations.add(1,3) == 4
+    assert Operations.add(1,4) == 5
 
 # def test_addition_negative():
 #     """Negative tests for addition"""
@@ -15,9 +15,9 @@ def test_addition_positive():
 
 def test_subtraction_positive():
     """Positive tests for subtraction"""
-    assert subtract(1,1) == 0
-    assert subtract(1,2) == -1
-    assert subtract(1,3) == -2
+    assert Operations.subtract(1,1) == 0
+    assert Operations.subtract(1,2) == -1
+    assert Operations.subtract(1,3) == -2
 
 # def test_subtraction_negative():
 #     """Negative tests for subtraction"""
@@ -27,9 +27,9 @@ def test_subtraction_positive():
 
 def test_multiplication_positive():
     """Positive tests for multiplication"""
-    assert multiply(1,1) == 1
-    assert multiply(1,2) == 2
-    assert multiply(1,3) == 3
+    assert Operations.multiply(1,1) == 1
+    assert Operations.multiply(1,2) == 2
+    assert Operations.multiply(1,3) == 3
 
 # def test_multiplication_negative():
 #     """Negative tests for multiplication"""
@@ -39,9 +39,9 @@ def test_multiplication_positive():
 
 def test_division_positive():
     """Positive tests for division"""
-    assert divide(1,1) == 1
-    assert divide(2,1) == 2
-    assert divide(12,2) == 6
+    assert Operations.divide(1,1) == 1
+    assert Operations.divide(2,1) == 2
+    assert Operations.divide(12,2) == 6
 
 # def test_division_negative():
 #     """Negative tests for division"""
@@ -50,18 +50,18 @@ def test_division_positive():
 #     assert divide(2,2) == 2
 
 def test_add():
-    assert add(1,1) == 2
+    assert Operations.add(1,1) == 2
 
 def test_subtract():
-    assert subtract(1,1) == 0
+    assert Operations.subtract(1,1) == 0
 
 def test_multiply():
-    assert multiply(1,1) == 1
+    assert Operations.multiply(1,1) == 1
 
 def test_divide_positive():
-    assert divide(1,1) == 1
+    assert Operations.divide(1,1) == 1
 
 def test_divide_negative():
     """Test dividing by zero"""
     with pytest.raises(ValueError, match="Division by zero is not allowed!"):
-        divide(1,0)
+        Operations.divide(1,0)
